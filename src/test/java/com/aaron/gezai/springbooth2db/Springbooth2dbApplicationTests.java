63e16dbf3cac82394ca56d12f6324dc1753ad3de
aagezai <aagezai@gmail.com>
2020-07-23 12:35:28 -0700

    spring boot and h2 database comitted

---
+++ src/test/java/com/aaron/gezai/springbooth2db/Springbooth2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaron.gezai.springbooth2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbooth2dbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
